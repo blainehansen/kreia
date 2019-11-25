5e69a60718f91b4b0a195b6eb73d8a4986cfbc97
Blaine Hansen <faichenshing@gmail.com>
2019-11-24 17:02:32 -0700

    more fleshed out

--- lib/IndentationLexerWithRawBlock.spec.ts
+++ lib/IndentationLexerWithRawBlock.spec.ts
@@ -53,6 +53,15 @@ describe('IndentationLexerWithRawBlock', () => {
 
 		expect(toks.length).eql(13)
 
+		const contents = toks.slice(3, 10)
+		expect((contents[0] as any).content).eql('\n\n')
+		expect((contents[1] as any).content).eql('jkfg ;l {}d 893 d#@# sdlfk\n')
+		expect((contents[2] as any).content).eql('     djfsadkf dfsadf asdfasf\n\n')
+		expect((contents[3] as any).content).eql('	sdfd\n')
+		expect((contents[4] as any).content).eql('	sdfkjd f;adfk\n')
+		expect((contents[5] as any).content).eql(' adf\n\n\n\n')
+		expect((contents[6] as any).content).eql('     ffdgadf\n')
+
 		expect(lexer.test([name])).false
 		expect(lexer.test([indent])).false
 		expect(lexer.test([deindent])).false
